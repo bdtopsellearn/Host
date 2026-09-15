d8673f114cd163c0893213143242a281dee60047
bdtopsellearn <mdjobayerhey@gmail.com>
2026-09-15 22:47:30 +0600

    Remove all files

--- laravel_VPS_reselling_website-main/bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\CustomerPanelProvider::class,
-    App\Providers\ProvisioningServiceProvider::class,
-];
